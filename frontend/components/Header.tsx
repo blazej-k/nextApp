e88378efb0294989042e2347da2324f4102edf2c
b.kania <blazej_kania>
2021-10-26 19:34:05 +0200

    create connecting between components and styles

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NextPage } from 'next'
 import { IDescription } from 'types'
+import styles from 'style/Header.module.scss'
 
 const Header: NextPage<IDescription> = ({description}) => {
     return(
-        <div className="Header">
+        <div className={styles.Header}>
             <div className="Header-title">
                 <h1>Header</h1>
             </div>
