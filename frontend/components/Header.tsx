3475db81f790db3aa05c20298d4218ec2095720f
b.kania <blazej_kania>
2021-10-26 20:01:30 +0200

    prepare header for animation

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useRef } from 'react'
 import { NextPage } from 'next'
 import { IDescription } from 'types'
 import styles from 'styles/Header.module.scss'
@@ -6,12 +6,24 @@ import styles from 'styles/Header.module.scss'
 const { Header_description } = styles
 
 const Header: NextPage<IDescription> = ({ description }) => {
+
+    const headerRef = useRef<HTMLDivElement>(null)
+
+    useLayoutEffect(() => {
+        const letters = headerRef.current?.textContent
+    }, [])
+
+    useEffect(() => {
+        const letters = headerRef.current?.textContent
+        console.log(letters)
+    }, [])
+
     return (
         <div className={styles.Header}>
             <div className="Header-title">
                 <h1>Header</h1>
             </div>
-            <div className={Header_description}>
+            <div className={Header_description} ref={headerRef}>
                 {description}
             </div>
         </div>
