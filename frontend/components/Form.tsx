b53c7ef028bb311e4d411544b934734dd47b174e
b.kania <blazej_kania>
2021-10-31 12:25:49 +0100

    form style

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,6 +1,7 @@
 import React, { MouseEvent, useReducer } from 'react'
 import { NextPage } from 'next'
 import { IFormAction, IFormInfo } from 'types'
+import styles from 'styles/Form.module.scss'
 
 interface IForm {
     haveUserAccount: boolean,
@@ -14,17 +15,17 @@ const initReducerState: IFormInfo = {
 }
 
 const formReducer = (state: IFormInfo = initReducerState, action: IFormAction) => {
-    switch(action.type){
+    switch (action.type) {
         case 'email':
-            return {...state, email: action.payload}
+            return { ...state, email: action.payload }
         case 'password':
-            return {...state, password: action.payload}
+            return { ...state, password: action.payload }
         default:
             throw new Error('Invalid action type')
     }
 }
 
-const Form: NextPage<IForm> = ({haveUserAccount, changeHaveUserAccount, handleSubmitForm}) => {
+const Form: NextPage<IForm> = ({ haveUserAccount, changeHaveUserAccount, handleSubmitForm }) => {
 
     const [form, dispatch] = useReducer(formReducer, initReducerState)
 
@@ -34,20 +35,20 @@ const Form: NextPage<IForm> = ({haveUserAccount, changeHaveUserAccount, handleSu
     }
 
     const handleInputChange = (arg: IFormAction) => dispatch(arg)
-    
-    return(
-        <div className="Form">
+
+    return (
+        <div className={styles.Form}>
             <h1>{haveUserAccount ? 'Sign in' : 'Sign up'}</h1>
             <form onSubmit={submitForm}>
                 <label htmlFor="email">E-mail:</label>
-                <input type="email"id='email' onChange={e => handleInputChange({type: 'email', payload: e.target.value})} />
+                <input type="email" id='email' onChange={e => handleInputChange({ type: 'email', payload: e.target.value })} />
                 <label htmlFor="password">Password:</label>
-                <input type="password" id='password' onChange={e => handleInputChange({type: 'password', payload: e.target.value})}/>
+                <input type="password" id='password' onChange={e => handleInputChange({ type: 'password', payload: e.target.value })} />
+                <button className={styles['Form-info']} onClick={changeHaveUserAccount}>
+                    I have not an account
+                </button>
                 <button type="submit">Sign in</button>
             </form>
-            <button className="Form-info" onClick={changeHaveUserAccount}>
-                I have not an account
-            </button>
         </div>
     )
 }
